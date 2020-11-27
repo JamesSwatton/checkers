caffc82092f2664234158eb67224068202792f6e
JamesSwatton <jamessswatton@gmail.com>
2020-11-27 16:04:02 +0000

    add better move calc logic

--- board.js
+++ board.js
@@ -10,26 +10,73 @@ const Board = {
         "01010101"
     ],
 
+    testLayout: [
+        "20202020",
+        "02020202",
+        "20202020",
+        "00000000",
+        "00200000",
+        "01010101",
+        "10101010",
+        "01010101"
+    ],
+
     pieces: [],
 
     // SETUP
     setup() {
         this.createPieces();
+        this.calcMoves();
         this.renderBoard();
         this.renderPieces();
     },
 
     createPieces() {
-        for (let y of this.layout) {
+        let layout = this.layout;
+        layout.forEach((row, i) => {
             let r = [];
-            for (let x of y) {
-                x == "0" ? r.push({ type: "blank" }) : r.push(newPiece(x));
-            }
+            row.split("").forEach((el, j) => {
+                el == "0"
+                    ? r.push({ type: "blank" })
+                    : r.push(newPiece(j, i, el));
+            });
             this.pieces.push(r);
-        }
+        });
     },
 
-    calcMoves() {},
+    calcMoves() {
+        this.pieces.forEach((row, y) => {
+            row.forEach((p, x) => {
+                if (p.type == "piece") p.clearMoves();
+                if (p.player == activePlayer) {
+                    p.paths.forEach(path => {
+                        if (
+                            // check for std move
+                            this.pathIsInBoard(x, y, path) &&
+                            this.pieces[y - path.y][x - path.x].type == "blank"
+                        ) {
+                            canMove = true;
+                            p.moves = `{"x":${x - path.x}, "y":${y - path.y}}`;
+                        } else if (
+                            // check for capture move
+                            this.pathIsInBoard(x, y, path) &&
+                            this.pathIsInBoard(x - path.x, y - path.y, path) &&
+                            this.pieces[y - path.y][x - path.x].player ==
+                                opponent &&
+                            this.pieces[y - path.y * 2][x - path.x * 2].type ==
+                                "blank"
+                        ) {
+                            canCapture = true;
+                            p.moves = [
+                                `{"x":${x - path.x}, "y":${y - path.y}}`,
+                                `{"x":${x - path.x * 2}, "y":${y - path.y * 2}}`
+                            ];
+                        }
+                    });
+                }
+            });
+        });
+    },
 
     // RENDERING
     renderBoard() {
@@ -70,6 +117,19 @@ const Board = {
                     pc.appendChild(b);
                 }
             }
+        }
+    },
+
+    // HELPERS
+
+    pathIsInBoard(x, y, path) {
+        if (
+            x - path.x >= 0 &&
+            x - path.x < 8 &&
+            y - path.y >= 0 &&
+            y - path.y < 8
+        ) {
+            return true;
         }
     }
 };
--- newPiece.js
+++ newPiece.js
@@ -1,9 +1,14 @@
-function newPiece(player) {
+function newPiece(x, y, player) {
     return {
+        x,
+        y,
         player,
         type: "piece",
         king: false,
-        moves: [],
+        _moves: {
+            std: [],
+            capture: []
+        },
         _paths: {
             "1": [
                 { x: -1, y: 1 },
@@ -19,6 +24,24 @@ function newPiece(player) {
         },
         get paths() {
             return this.king ? this._paths.king : this._paths[this.player];
+        },
+        get moves() {
+            if (this._moves.capture.length > 0) {
+                return this._moves.capture;
+            } else if (this._moves.std.length > 0) {
+                return this._moves.std;
+            } else {
+                return null;
+            }
+        },
+        set moves(m) {
+            typeof m == "object"
+                ? this._moves.capture.push(m)
+                : this._moves.std.push(m);
+        },
+        clearMoves() {
+            this._moves.std = [];
+            this._moves.capture = [];
         }
     };
 }
